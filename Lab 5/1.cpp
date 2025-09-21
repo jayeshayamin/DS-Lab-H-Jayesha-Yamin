a4f7443b61a437bb5639398f9adeed611f4da95d
jayeshayamin <jayeshayamin1@gmail.com>
2025-09-21 22:34:06 +0500

    Create 1.cpp

---
+++ Lab 5/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int calculatefactorial(int n)
+{
+    if(n==1)
+    return 1;
+    if(n==0)
+    return 1;
+    return n*calculatefactorial(n-1);
+}
+int main() {
+    int n;
+    cout<<"Enter factorial: ";
+    cin>>n;
+    cout<<"Calculated facotiral: "<<calculatefactorial(n);
+    return 0;
+}
